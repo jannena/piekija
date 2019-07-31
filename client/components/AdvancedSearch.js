6c33296baa63debfa54c22efcad727153c2995ce
Jannena <janne.jalone@outlook.com>
2019-07-31 19:22:00 +0300

    Updated advanced search

--- client/components/AdvancedSearch.js
+++ client/components/AdvancedSearch.js
@@ -9,6 +9,7 @@ const QueryGroupStyle = {
 };
 
 const AdvancedSearchField = ({ query }) => {
+    // TODO: onChange function
     return (
         <div>
             <input value={query[1]} />
@@ -24,19 +25,44 @@ const AdvancedSearchField = ({ query }) => {
     );
 };
 
+// This is a recursive component
 const AdvancedSearchGroup = ({ query, setQuery }) => {
     if (Array.isArray(query[1])) {
+        // Event handlers for new group and field addition
+        const addToThisLevel = toBeAdded => {
+            const newQuery = [...query];
+            newQuery[1].push(toBeAdded);
+            setQuery(newQuery);
+        };
+        const addNewField = () => addToThisLevel(["record", "", "contains"]);
+        const addNewGroup = () => addToThisLevel(["and", []]);
+
+        console.log(setQuery.toString());
+
         return (
             <div style={QueryGroupStyle}>
-                <button>Add new field to this group</button>
-                <button>Add new group to this group</button>
+                <button onClick={addNewField}>Add new FIELD to this group</button>
+                <button onClick={addNewGroup}>Add new GROUP to this group</button>
                 <Select
                     options={[["with all these (and)", "and"], ["with any of these (or)", "or"]]}
                     selected={query[0]}
                 />
-                {query[1].map(q => Array.isArray(q[1])
-                    ? <AdvancedSearchGroup query={q} />
-                    : <AdvancedSearchField query={q} />
+                {/* If, second element of the array is an array, element is a group.
+                    Else, element is a field
+                */}
+                { /* TODO: DO NOT USE INDEX AS A KEY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
+                {query[1].map((q, i) => Array.isArray(q[1])
+                    ? <AdvancedSearchGroup
+                        key={i}
+                        query={q}
+                        // Since this is a recursive component, we must create a new 'setQuery' function
+                        setQuery={newQuery => {
+                            const updatedQuery = query[i][1];
+                            updatedQuery.push(newQuery);
+                            setQuery(updatedQuery);
+                        }}
+                    />
+                    : <AdvancedSearchField key={i} query={q} />
                 )}
             </div>
         );
@@ -46,8 +72,9 @@ const AdvancedSearchGroup = ({ query, setQuery }) => {
     }
 };
 
-const AdvancedSearch = () => {
-    const [query, setQuery] = useState(["or", [
+
+/*
+"or", [
         ["authors", "Jansson, Tove", "is"],
         ["title", "Kesäkirja", "contains"],
         ["and", [
@@ -58,13 +85,28 @@ const AdvancedSearch = () => {
             ["title", "Juuson", "is"],
             ["authors", "Maikku", "contains"],
         ]]
-    ]]);
+    ]
+*/
+const AdvancedSearch = () => {
+    const [query, setQuery] = useState(["and", []]);
 
     console.log(query);
 
+    const setQueryMock = newQuery => {
+        console.log("setted a new query", newQuery);
+        return setQuery(newQuery);
+    }
+
     return (
         <div>
-            <AdvancedSearchGroup query={query} />
+            <AdvancedSearchGroup
+                query={query}
+                setQuery={setQueryMock}
+                setQuery={newQuery => {
+                    const updatedQuery = [...query, newQuery];
+                    setQueryMock(updatedQuery);
+                }}
+            />
         </div>
     );
 };
