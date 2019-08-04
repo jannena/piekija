7bf2f9a8b57e86f521f992179ff04dc52a9a1622
Jannena <janne.jalone@outlook.com>
2019-08-04 18:53:37 +0300

    Updated tests for record controller

--- server/tests/api/record.test.js
+++ server/tests/api/record.test.js
@@ -4,7 +4,7 @@ const supertest = require("supertest");
 
 const api = supertest(app);
 
-const { addUserToDb, clearDatabase, getTokenForUser, initMARC21Data } = require("./testutils");
+const { addUserToDb, clearDatabase, getTokenForUser, initMARC21Data, recordsInDb, addRecordToDb } = require("./testutils");
 
 let staffToken = "";
 let token = "";
@@ -18,6 +18,7 @@ beforeAll(async () => {
 describe("record tests", () => {
     describe("when non-staff user is logged in", () => {
         test("record can be added in marc21 format and it will be read correctly", async () => {
+            const recordsAtStart = await recordsInDb();
             const res = await api
                 .post("/api/record")
                 .set({ Authorization: `Bearer ${staffToken}` })
@@ -29,6 +30,7 @@ describe("record tests", () => {
                 .expect("Content-type", /application\/json/);
 
             // console.log("response", res);
+            expect(await recordsInDb()).toBe(recordsAtStart + 1);
 
             // TODO: Remove last chars (/ and , etc.)
             expect(res.body.title).toBe("Liitu-ukko /");
@@ -52,15 +54,107 @@ describe("record tests", () => {
             ]);
             expect(res.body.year).toBe(2018);
             expect(res.body.id).toBeDefined();
-            expect(res.body.id).not.toBeDefined();
+            expect(res.body._id).not.toBeDefined();
             expect(res.body.__v).not.toBeDefined();
         });
 
-        test("record can be edited with new marc21 data", () => {});
-        test("record can be removed", () => {});
-        test("removing record removes also items that are attached to the record", () => {});
+        describe("and when there is records in the databse", () => {
+            const recordId = "";
 
-        // TODO: Check whether there is actually a new record in db. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            beforeEach(async () => {
+                recordId = await addRecordToDb();
+            });
+
+            test("record can be edited with new marc21 data", async () => {
+                const recordsAtStart = await recordsInDb();
+                const res = await api
+                    .put(`/api/record/${recordId}`)
+                    .send(initMARC21Data[1])
+                    .expect(200)
+                    .expect("Content-type", /application\/json/);
+
+                expect(await recordId()).toBe(recordsAtStart);
+                expect(res.body.id).toBeDefined();
+                expect(res.body._id).not.toBeDefined();
+                expect(res.body.__v).not.toBeDefined();
+
+                // TODO: Remove last characters
+                expect(res.body.title).toBe("Imaginaerum /");
+                expect(res.body.authors).toEqual([
+                    "Nightwish,",
+                    "Shearman, James,"
+                ]);
+
+                expect(res.body.subjects).toEqual([
+                    "heavy-rock",
+                    "rock",
+                    "progressiivinen rock",
+                    "kuorot",
+                    "orkesterit",
+                    "kelttiläinen kansanmusiikki"
+                ]);
+                expect(res.body.year).toBe(2011);
+            });
+            test("record can be removed", async () => {
+                const recordsAtStart = await recordsInDb();
+                await api
+                    .delete(`/api/record/${recordId}`)
+                    .expect(204);
+                expect(await recordsInDb()).toBe(recordsAtStart - 1);
+            });
+            test("record cannot be removed if there is items arttached to the record", () => {
+                // TODO: Do the test
+            });
+        });
+    });
+
+    describe("when non-staff user is logged in", () => {
+        test("new record cannot be added", async () => {
+            const recordAtStart = await recordsInDb();
+            await api
+                .post("/api/record")
+                .send(initMARC21Data[0])
+                .expect(403);
+
+            expect(await recordsInDb()).toBe(recordAtStart);
+        });
+
+        describe("and when there is record in database", () => {
+            const recordId = "";
+
+            beforeEach(async () => {
+                recordId = await addRecordToDb();
+            });
+
+            test("record can be received", async () => {
+                const res = await api
+                    .get(`/api/record/${recordId}`)
+                    .expect(200)
+                    .expect("Content-type", /application\/json/);
+
+                expect(res.body.id).toBeDefined();
+                expect(res.body._id).not.toBeDefined();
+                expect(res.body.__v).not.toBeDefined();
+                expect(res.body.title).toBe("A New Book");
+            });
+
+            test("record cannot be removed", async () => {
+                const recordAtStart = await recordsInDb();
+                await api
+                    .delete(`/api/record/${recordId}`)
+                    .expect(403);
+
+                expect(await recordsInDb()).toBe(recordAtStart);
+            });
+
+            test("record cannot be edited", async () => {
+                await api
+                    .put(`/api/record/${recordId}`)
+                    .send(initMARC21Data[0])
+                    .expect(403);
+                // TODO: Check whether there is actually no changes in the database!
+            });
+        });
     });
 
     describe("when user is not logged in", () => {
@@ -72,8 +166,40 @@ describe("record tests", () => {
             expect(res.body.error).toBe("you must login first");
         });
 
-        test("record cannot be removed", () => {});
-        test("record cannot be edited", () => {});
+        describe("and when there is record in database", () => {
+            const recordId = "";
+
+            beforeEach(async () => {
+                recordId = await addRecordToDb();
+            });
+
+            test("record can be received", async () => {
+                const res = await api
+                    .get(`/api/record/${recordId}`)
+                    .expect(200)
+                    .expect("Content-type", /application\/json/);
+
+                expect(res.body.id).toBeDefined();
+                expect(res.body.title).toBe("A New Book");
+            });
+
+            test("record cannot be removed", async () => {
+                const recordAtStart = await recordsInDb();
+                await api
+                    .delete(`/api/record/${recordId}`)
+                    .expect(401);
+
+                expect(await recordsInDb()).toBe(recordAtStart);
+            });
+
+            test("record cannot be edited", async () => {
+                await api
+                    .put(`/api/record/${recordId}`)
+                    .send(initMARC21Data[0])
+                    .expect(401);
+                // TODO: Check whether there is actually no changes in the database!
+            });
+        });
     });
 });
 
--- server/tests/api/testutils.js
+++ server/tests/api/testutils.js
@@ -1,10 +1,12 @@
 const { SECRET } = require("../../utils/config");
 const User = require("../../models/User");
+const Record = require("../../models/Record");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const initMARC21Data = [
-    "02021cam a22005774i 45000010008000000030011000080050017000190080041000360200031000770350025001080350026001330400035001590410013001940420009002070800030002160840014002460840019002601000032002792400026003112450060003372640057003972640011004542640011004653000023004763360028004993370049005273380025005766000031006016500026006326500036006586500032006946500020007266500036007466550049007826550036008316550029008676550023008966550079009197000060009988520015010588520015010738520015010888520015011038520015011188520015011338520015011488520014011638520015011778560126011928560125013181706985ANDL10001320181011151338.4180223t20182018fi ||||g      |0| f fin    a978-951-0-42529-9qsidottu  a(FI-KV)9789510425299  a(FI-MELINDA)012207067  aFI-NLbfinerdadFI-BTJdFI-KM1 afinheng  afinb1 a820x-321974/fin/fennica  a84.22ykl9 aJännitys2ykl1 aTudor, C. J.,ekirjoittaja.14aThe chalk man,lsuomi10aLiitu-ukko /cC. J. Tudor ; suomentanut Raimo Salminen. 1aHelsinki :bWerner Söderström osakeyhtiö,c[2018] 3aEU:ssa 4c©2018  a426 sivua ;c23 cm  atekstibtxt2rdacontent  akäytettävissä ilman laitettabn2rdamedia  anidebnc2rdacarrier04aEddiec(fiktiivinen hahmo) 7asalaisuudet2kaunokki 7aviestitxpiirustukset2kaunokki 7adéjà vu -ilmiö2kaunokki 7amurha2kaunokki 7aaikatasoty1986y20162kaunokki 7apsykologinen jännityskirjallisuus2kaunokki 7ajännityskirjallisuus2kaunokki 7aesikoisteokset2kaunokki 7aromaanit2kaunokki 7akaunokirjallisuusxenglanninkielinen kirjallisuusxkäännökset2kaunokki1 aSalminen, Raimo,ekääntäjä.0(FI-ASTERI-N)000058058  a18b001c3  a18b003c3  a18b201c3  a19b401c3  a18b501c3  a10b601c3  a12b701c3  a9b801c3  a18b901c342qimage/jpeguhttp://data.kirjavalitys.fi/data/servlets/ProductRequestServlet?action=getimage&ISBN=9789510425299zKansikuva42qtext/htmluhttp://data.kirjavalitys.fi/data/servlets/ProductRequestServlet?action=showreferat&ISBN=9789510425299zKuvaus"
+    "02021cam a22005774i 45000010008000000030011000080050017000190080041000360200031000770350025001080350026001330400035001590410013001940420009002070800030002160840014002460840019002601000032002792400026003112450060003372640057003972640011004542640011004653000023004763360028004993370049005273380025005766000031006016500026006326500036006586500032006946500020007266500036007466550049007826550036008316550029008676550023008966550079009197000060009988520015010588520015010738520015010888520015011038520015011188520015011338520015011488520014011638520015011778560126011928560125013181706985ANDL10001320181011151338.4180223t20182018fi ||||g      |0| f fin    a978-951-0-42529-9qsidottu  a(FI-KV)9789510425299  a(FI-MELINDA)012207067  aFI-NLbfinerdadFI-BTJdFI-KM1 afinheng  afinb1 a820x-321974/fin/fennica  a84.22ykl9 aJännitys2ykl1 aTudor, C. J.,ekirjoittaja.14aThe chalk man,lsuomi10aLiitu-ukko /cC. J. Tudor ; suomentanut Raimo Salminen. 1aHelsinki :bWerner Söderström osakeyhtiö,c[2018] 3aEU:ssa 4c©2018  a426 sivua ;c23 cm  atekstibtxt2rdacontent  akäytettävissä ilman laitettabn2rdamedia  anidebnc2rdacarrier04aEddiec(fiktiivinen hahmo) 7asalaisuudet2kaunokki 7aviestitxpiirustukset2kaunokki 7adéjà vu -ilmiö2kaunokki 7amurha2kaunokki 7aaikatasoty1986y20162kaunokki 7apsykologinen jännityskirjallisuus2kaunokki 7ajännityskirjallisuus2kaunokki 7aesikoisteokset2kaunokki 7aromaanit2kaunokki 7akaunokirjallisuusxenglanninkielinen kirjallisuusxkäännökset2kaunokki1 aSalminen, Raimo,ekääntäjä.0(FI-ASTERI-N)000058058  a18b001c3  a18b003c3  a18b201c3  a19b401c3  a18b501c3  a10b601c3  a12b701c3  a9b801c3  a18b901c342qimage/jpeguhttp://data.kirjavalitys.fi/data/servlets/ProductRequestServlet?action=getimage&ISBN=9789510425299zKansikuva42qtext/htmluhttp://data.kirjavalitys.fi/data/servlets/ProductRequestServlet?action=showreferat&ISBN=9789510425299zKuvaus",
+    "02470njm_a22006134i_45000010008000000030011000080050017000190070015000360080041000510240018000920280016001100350020001260350026001460400008001720410013001800840018001930840017002111100028002282450030002562460025002862460029003112600058003403000087003983360039004853370023005243380031005475000038005785050041006165110350006575960010010076500039010176500033010566500049010896500029011386500033011676500051012007000031012518520015012828520015012978520015013129790036013279790035013639790037013989790042014359790046014779790035015239790035015589790049015939790035016429790056016779790046017339790040017799790037018191247533ANDL10001320180613110936.0sd |||g|||m||d111129s2011    fi rc||__||||||__ | eng||3 a643004379002802aSCENECD-002  a(FI-BTJ)1652326  a(FI-MELINDA)007301531  bfin0 dengdfin  a78.891122ykl  a78.89112ykl2 aNightwish,eesittäjä.10aImaginaerum /cNightwish.33aImaginaerum original33aImaginaerum instrumental  a[Kustannuspaikka tuntematon] :bScene Nation,cp2011.  a2 CD-äänilevyä (74 min 56 s, 73 min 56 s) +etekstiliite (24 s.) + 2 julistetta  aesitetty musiikkibprm2rdacontent  aaudiobs2rdamedia  aäänilevybsd2rdacarrier  aLaulujen sanat tekstiliitteessä.0 aCD 1: Original ; CD 2: Instrumental.0 aNightwish: Anette Olzon (voc), Marco Hietala (b, voc), Emppu Vuorinen (g), Jukka Nevalainen (dr, perc), Tuomas Holopainen (keys, p) & The Metro Voices, The Young Musicians London, The Looking Glass Orchestra, Pekka Kuusisto (v), Troy Donockley (säkkipilli, low whistle, bodhran, buzuki), Dermot Crehan (hardangerviulu), Dirk Campbell (sorna)...  bLO506 7aheavy rockzSuomiy2010-luku2musa 7arockzSuomiy2010-luku2musa 7aprogressiivinen rockzSuomiy2010-luku2musa 7akuorotxheavy rock2musa 7aorkesteritxheavy rock2musa 7akelttiläinen kansanmusiikkixheavy rock2musa1 aShearman, James,ejohtaja.  a18b001c4  a19b401c4  a18b501c4  aanders.1261392bTaikatalvihfin  aanders.1261393bStorytimeheng  aanders.1261394bGhost riverheng  aanders.1261395bSlow, love, slowheng  aanders.1261396bI want my tears backheng  aanders.1261397bScaretaleheng  aanders.1261398bArabesqueheng  aanders.1261399bTurn loose the mermaidsheng  aanders.1261400bRest calmheng  aanders.1261401bThe crow, the owl and the doveheng  aanders.1261402bLast ride of the dayheng  aanders.1261403bSong of myselfheng  aanders.1261404bImaginaerumheng"
 ];
 
 const addUserToDb = async (username, password, staff) => {
@@ -21,6 +23,44 @@ const addUserToDb = async (username, password, staff) => {
     return await newUser.save();
 };
 
+const addRecordToDb = async () => {
+    const newRecord = new Record({
+        timeAdded: new Date(),
+        timeModified: new Date(),
+        title: "A New Book",
+        description: "",
+        image: "",
+        contentType: "a",
+        year: 2019,
+        author: "Virtanen, Juhani,",
+        authors: [
+            "Virtanen, Juhani,",
+            "Virtanen, Maria,"
+        ],
+        genres: [
+            "fantasiakirjallisuus",
+            "tietieskirjallisuus"
+        ],
+        subjects: [
+            "maailma",
+            "Eurooppa",
+            "ruoho",
+            "puutarha"
+        ],
+        language: "fin",
+        languages: [
+            "fin",
+            "eng"
+        ],
+        links: [],
+        recordType: "marc21",
+        record: "",
+        items: []
+    });
+    const savedRecord = await newRecord.save();
+    return savedRecord._id;
+};
+
 const getTokenForUser = user => {
     const tokenData = {
         id: user._id,
@@ -31,12 +71,19 @@ const getTokenForUser = user => {
 };
 
 const clearDatabase = async () => {
+    await Record.deleteMany({});
     await User.deleteMany({});
-}
+};
+
+const usersInDb = async () => (await User.find({})).length;
+const recordsInDb = async () => (await Record.find({})).length;
 
 module.exports = {
     addUserToDb,
+    addRecordToDb,
     clearDatabase,
     getTokenForUser,
-    initMARC21Data
+    initMARC21Data,
+    usersInDb,
+    recordsInDb
 };
