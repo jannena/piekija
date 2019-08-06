0bbcdfccb9c650821c4ab955474387c81708e274
jannena <janne.jalone@outlook.com>
2019-08-06 17:55:37 +0300

    Added toJSON

--- server/models/User.js
+++ server/models/User.js
@@ -39,4 +39,12 @@ const userSchema = new mongoose.Schema({
     ]
 });
 
+userSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    }
+});
+
 module.exports = mongoose.model("User", userSchema)
