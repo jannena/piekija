e20daacc391bb6c14aee32439b0b812d5975e158
jannena <janne.jalone@outlook.com>
2019-08-06 18:34:33 +0300

    Added unique error notifier

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -2,6 +2,7 @@
 const errorHandler = (error, req, res, next) => {
     if (error.name === "JsonWebTokenError") return res.status(401).json({ error: "invalid token" });
     if (error.name === "CastError") return res.status(400).json({ error: "malformatted id" });
+    if (error.name === "MongoError" && error.message.includes("duplicate key error")) return res.status(400).json({ error: "something must be unique"  });
     if (error.message === "UNAUTHORIZED") return res.status(401).json({ error: "you must login first" });
     if (error.message === "FORBIDDEN") return res.status(403).json({ error: "you do not have permission to do that" });
     
