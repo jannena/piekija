30e3fa4af49739eac407d590026152ad804732f8
jannena <janne.jalone@outlook.com>
2019-08-08 16:37:38 +0300

    Added two-factor authentication

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -3,17 +3,30 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("../utils/config");
+const otp = require("speakeasy");
 
 loginRouter.post("/", async (req, res, next) => {
     try {
-        const { username, password } = req.body;
+        const { username, password, code } = req.body;
         if (!username || !password) return res.status(400).json({ error: "username or password is missing" });
 
         const user = await User.findOne({ username });
         if (!user) return res.status(401).json({ error: "wrong username or password" });
 
         const passwordCorrect = await bcrypt.compare(password, user.passwordHash);
-        if (!passwordCorrect) return res.status(401).json({ error: "wring username or password" });
+        if (!passwordCorrect) return res.status(401).json({ error: "wrong username or password" });
+
+        if (user.TFACode) {
+            if (!code) return res.status(400).json({ error: "code needed" });
+            else {
+                const verified = otp.totp.verify({
+                    encoding: "base32",
+                    secret: user.TFACode,
+                    token: code
+                })
+                if (!verified) return res.status(401).json({ error: "invalid code" });
+            }
+        }
 
         const tokenData = {
             id: user._id,
--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    TFACode: String,
     username: {
         type: String,
         required: true,
