6f5db50e59fd3178639571e0fb6b103159685433
jannena <janne.jalone@outlook.com>
2019-08-10 16:52:31 +0300

    Updated: me endpoint returns qrcode

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,6 +2,7 @@ const userRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const otp = require("speakeasy");
+const QRCode = require("qrcode");
 
 userRouter.get("/me", async (req, res, next) => {
     if (!req.authenticated) return next(new Error("UNAUTHORIZED"));
@@ -30,27 +31,35 @@ userRouter.put("/me", async (req, res, next) => {
     if (!oldPassword) return res.status(400).json({ error: "oldPassword is missing" });
 
     try {
+        const passwordCorrect = await bcrypt.compare(oldPassword, req.authenticated.passwordHash);
+        if (!passwordCorrect) return res.status(403).json({ error: "wrong oldPassword" });
+
+        // Generate new secret for two-factor authentication
+        if (tfa === true) {
+            const secret = otp.generateSecret({ encoding: "base32" });
+            await User.findByIdAndUpdate(req.authenticated._id, { $set: { TFACode: secret.base32 } })
+            return QRCode.toDataURL(secret.otpauth_url, (err, url) => {
+                if (err) return next(err);
+                res.json({ TFAQR: url });
+            });
+        }
+        // Clear two-factor authentication
+        else if (tfa === false) usertoBeSaved.TFACode = "";
+        else if (tfa !== undefined) return res.status(400).json({ error: "tfa must be true or false" });
+
+        // Everything else but two-factor authentication
         const usertoBeSaved = {};
 
         if (password && password.length < 10) return res.status(400).json({ error: "password too short" });
         if (password) usertoBeSaved.passwordHash = await bcrypt.hash(password, 13);
 
-        // Generate new secret for two-factor authentication
-        if (tfa === true) usertoBeSaved.TFACode = otp.generateSecret({ encoding: "base32" }).base32;
-        // Clear two-factor authentication
-        else if (tfa === false) usertoBeSaved.TFACode = "";
-        else if (!name && !password) return res.status(400).json({ error: "tfa must be true or false" });
-
         if (name) usertoBeSaved.name = name;
-
-        const passwordCorrect = await bcrypt.compare(oldPassword, req.authenticated.passwordHash);
-        if (!passwordCorrect) return res.status(403).json({ error: "wrong oldPassword" });
 
         const modifiedUser = await User.findByIdAndUpdate(req.authenticated.id, { $set: usertoBeSaved }, { new: true });
         await User.populate(modifiedUser, {
             path: "shelves.id",
             select: "name"
-        })
+        });
         res.json(modifiedUser.toJSON());
     }
     catch (err) {
