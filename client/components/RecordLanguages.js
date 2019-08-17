52981318ee0ccdc792280e5b7a681a5d01458400
jannena <janne.jalone@outlook.com>
2019-08-17 22:40:41 +0300

    Added main language to record screen

--- client/components/RecordLanguages.js
+++ client/components/RecordLanguages.js
@@ -4,8 +4,17 @@ const MARC21 = require("../../server/utils/marc21parser");
 
 const RecordLanguages = ({ record }) => {
     const langs = MARC21.getFieldsAndSubfields(record.record, ["041"], ["a", "b", "d", "f", "g", "h", "j", "k"]);
+    const mainLanguage = (() => {
+        try {
+            return record.record.FIELDS["008"][0].substring(35, 38);
+        }
+        catch (err) {
+            return false;
+        }
+    })();
 
     return (<>
+        {mainLanguage && <div>Main language: {mainLanguage}</div>}
         {langs.map(lang => <>
             <div>{!!lang["a"].length && "Text: " + lang["a"].join(", ")}</div>
             <div>{!!lang["b"].length && "Summar or abstract: " + lang["b"].join(", ")}</div>
