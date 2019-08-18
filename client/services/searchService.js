c7f1a0a71d3c0713463eecbd9815791dc48036fc
jannena <janne.jalone@outlook.com>
2019-08-18 16:49:42 +0300

    Combined advanced and simple search

--- client/services/searchService.js
+++ client/services/searchService.js
@@ -2,15 +2,10 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:3001/api/search";
 
-const simpleSearch = (query, page) => {
-    return axios.post(`${baseUrl}/simple`, { query, page }).then(response => response.data);
-};
-
-const advancedSearch = (query, page) => {
-    return axios.post(`${baseUrl}/advanced`, { query, page }).then(response => response.data);
+const search = (query, page, sort, advanced) => {
+    return axios.post(`${baseUrl}/${advanced ? "advanced" : "simple"}`, { query, page }).then(response => response.data);
 };
 
 export default {
-    simpleSearch,
-    advancedSearch
-}
+    search
+};
