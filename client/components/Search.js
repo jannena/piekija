85662731c04b560a869999448870c0640780d4e5
jannena <janne.jalone@outlook.com>
2019-08-18 17:48:08 +0300

    Fixed pagination buttons and simplified Search component

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,64 +1,16 @@
-import React, { useState, useEffect } from "react";
-import searchService from "../services/searchService";
+import React from "react";
 import SearchField from "./SearchField";
-import qs from "query-string";
-import { Link } from "react-router-dom";
 import AdvancedSearch from "./AdvancedSearch";
 import Select from "./Select";
 import RecordPreview from "./RecordPreview";
 import { connect } from "react-redux";
-import { advancedSearch } from "../reducers/searchReducer";
+import { nextPage, previousPage } from "../reducers/queryReducer";
 
-const Search = ({ queryParams, history, result, advancedSearch }) => {
-    const { type, q: query, page: p } = qs.parse(queryParams);
-    const page = !p ? 1 : Number(p);
+const Search = ({ result, page, nextPage, previousPage }) => {
 
     console.log("rendered search");
-
-    useEffect(() => {
-        if (query) {
-            if (type === "advanced") {
-                try {
-                    onAdvancedSearch(JSON.parse(query));
-                }
-                catch (err) {
-                    console.log(err);
-                }
-            }
-            else onSearch(query);
-        }
-    }, [query, page]);
-
-    const onSearch = query => {
-        history.push({
-            pathname: "/search",
-            search: `?type=simple&q=${encodeURIComponent(query)}&page=${page}`
-        });
-        searchService
-            .simpleSearch(query, page)
-            .then(result => {
-                console.log("haku", JSON.stringify(result.query));
-                setResult(result);
-            })
-            .catch(err => {
-                console.log("VIRHE hakutuloksia haettaessa", err, err.message);
-            });
-        console.log("requested simple search results");
-    };
-
-    const onAdvancedSearch = query => {
-        // TODO: This mus be done in somewhere
-        history.push({
-            pathname: "/search",
-            search: `?type=advanced&q=${encodeURIComponent(JSON.stringify(query))}&page=${page}`
-        });
-    };
-
     console.log("sivulla", page, !page || page === 1);
     console.log("doNotShowNextPageLink?", result.found / 3 <= page);
-
-    const previousPageLink = () => !page || page === 1 ? null : <Link to={`/search?type=${type}&q=${query}&page=${page - 1}`}>&lt;&lt; Previous</Link>;
-    const nextPageLink = () => result.found / 3 <= page ? null : <Link to={`/search?type=${type}&q=${query}&page=${page + 1}`}>Next &gt;&gt;</Link>
 
     return (
         <div>
@@ -66,22 +18,26 @@ const Search = ({ queryParams, history, result, advancedSearch }) => {
             <AdvancedSearch />
             <hr />
             {/* Order by {<Select options={[["default", "default"], ["time added", "timeAdded"], ["year", "year"], ["alphapetical", "a"]]} />} */}
-            <p>Found {result.found} records in {(result.time || NaN).toFixed(0)} milliseconds</p>
+
             {result.result.length === 0
-                ? (!query ? "^" : `No results for ${query}`)
-                : result.result.map(record => <RecordPreview key={record.id} record={record} />)}
-            <p>
-                {previousPageLink()}
-                | Page {page} |
-                {nextPageLink()}
-            </p>
+                ? "No results"
+                : <>
+                    <p>Found {result.found} records in {(result.time || NaN).toFixed(0)} milliseconds</p>
+                    {result.result.map(record => <RecordPreview key={record.id} record={record} />)}
+                    <p>
+                        {page >= 2 && <button onClick={previousPage}>&lt;&lt; previous</button>}
+                        | Page {page} |
+                        {<button onClick={nextPage}>next &gt;&gt;</button>}
+                    </p>
+                </>}
         </div>
     );
 };
 
 export default connect(
     state => ({
-        result: state.search.result
+        result: state.search.result,
+        page: state.query.page
     }),
-    { advancedSearch }
+    { nextPage, previousPage }
 )(Search);
