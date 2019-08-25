b215626741347611cb3e3f99e7875098def903e1
jannena <janne.jalone@outlook.com>
2019-08-25 13:20:59 +0300

    Updated possibility to edit subfield data in record editor

--- client/components/staff/RecordEditor.js
+++ client/components/staff/RecordEditor.js
@@ -1,49 +1,87 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { getRecord, updateRecord } from "../../reducers/recordReducer";
 
-const RecordEditor = ({ id, record, getRecord }) => {
+const MARC21 = require("../../../server/utils/marc21parser");
+
+const RecordEditor = ({ id, record, getRecord, updateRecord }) => {
     console.log("editing", record);
+
+    const [editedRecord, setEditedRecord] = useState(null);
 
     useEffect(() => {
         console.log(id);
         getRecord(id);
     }, [id]);
 
-    if (!record.record) return null;
+    useEffect(() => {
+        if (record && record.record && record.record.FIELDS) setEditedRecord({
+            ...record.record,
+            FIELDS: Object.entries(record.record.FIELDS).sort(([a], [b]) => Number(a) - Number(b))
+        });
+    }, [record]);
 
-    const marcData = {
-        ...record.record,
-        FIELDS: Object.entries(record.record.FIELDS).sort(([a], [b]) => Number(a) - Number(b))
+    if (!record.record || !editedRecord) return null;
+
+    const onAddField = () => { };
+    const onAddSubfield = () => { };
+    const onSubfieldChange = (field, i, subfield, n) => e => {
+        // const edited = {
+        //     ...editedRecord,
+        //     FIELDS: editedRecord.FIELDS.map(([code, data]) => code === field ? [
+        //         [code, data.map((value, i) => )]
+        //     ] : [code, data])
+        // };
+        console.log("tried to edit", field, i, subfield, n, editedRecord.FIELDS[field][1][i]);
+        const copy = { ...editedRecord };
+        copy.FIELDS[field][1][i].subfields[subfield][n] = e.target.value;
+        setEditedRecord(copy);
+    };
+    const onIndicatorChange = (field, i, indicator, n) => { };
+    const onRemoveSubfield = () => { };
+    const onSave = e => {
+        // e.preventDefault();
+        const joo = MARC21.stringify({
+            ...editedRecord,
+            FIELDS: Object.fromEntries(editedRecord.FIELDS)
+        });
+        console.log(joo);
+        updateRecord(record.result.id, joo);
     };
 
     // TODO: ADD KEYS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     return (
         <table>
-            {marcData.FIELDS.map(([code = "000", fieldsData = {}]) => <React.Fragment key={code}>
-                {fieldsData.map(fieldData => <React.Fragment key={JSON.stringify(fieldData)}>
-                    {console.log(fieldData, fieldData.subfields)}
-                    <tr>
-                        <td>{code}</td>
-                        <td>
-                            <input value={fieldData.indicators && fieldData.indicators[0]} />
-                            <input value={fieldData.indicators && fieldData.indicators[1]} />
-                        </td>
-                    </tr>
-                    {fieldData.subfields && Object.entries(fieldData.subfields).map(([subfieldCode, subfieldData]) =>
-                        <React.Fragment>
-                            <tr>
-                                <td>{subfieldCode}</td>
-                            </tr>
-                            {subfieldData.map(value => <tr>
-                                <td></td>
-                                <td>
-                                    <input value={value} />
-                                </td>
-                            </tr>)}
-                        </React.Fragment>)}
+            <tbody>
+                {editedRecord.FIELDS.map(([code = "000", fieldsData = {}], field) => <React.Fragment key={code}>
+                    {fieldsData.map((fieldData, i) => <React.Fragment key={i}>
+                        {console.log(fieldData, fieldData.subfields)}
+                        <tr>
+                            <td>{code}</td>
+                            <td>
+                                {fieldData.indicators ? <>
+                                    <input value={fieldData.indicators[0]} />
+                                    <input value={fieldData.indicators[1]} />
+                                </>
+                                    : <input value={fieldData} />}
+                            </td>
+                        </tr>
+                        {fieldData.subfields && Object.entries(fieldData.subfields).map(([subfieldCode, subfieldData]) =>
+                            <React.Fragment key={subfieldCode}>
+                                <tr>
+                                    <td>{subfieldCode}</td>
+                                </tr>
+                                {subfieldData.map((value, n) => <tr key={n}>
+                                    <td></td>
+                                    <td>
+                                        <input value={value} onChange={onSubfieldChange(field, i, subfieldCode, n)} />
+                                    </td>
+                                </tr>)}
+                            </React.Fragment>)}
+                    </React.Fragment>)}
                 </React.Fragment>)}
-            </React.Fragment>)}
+                <tr><td colSpan={3}><button onClick={onSave}>Save</button></td></tr>
+            </tbody>
         </table>
     );
 };
