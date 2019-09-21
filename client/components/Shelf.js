d9332b32adf944f29d43c279fa7282c6e094b2f5
jannena <janne.jalone@outlook.com>
2019-09-21 19:47:53 +0300

    Updated shelf component layout

--- client/components/Shelf.js
+++ client/components/Shelf.js
@@ -4,6 +4,7 @@ import ShelfRecord from "./ShelfRecord";
 import ShelfSharing from "./ShelfSharing";
 import { connect } from "react-redux";
 import { getShelf, updateShelf } from "../reducers/shelfReducer";
+import { Tabs, Tab } from "./Tabs";
 import io from "../socket";
 
 const Shelf = ({ state, shelfId, shelf, token, user, getShelf, updateShelf }) => {
@@ -31,39 +32,48 @@ const Shelf = ({ state, shelfId, shelf, token, user, getShelf, updateShelf }) =>
     };
 
     return (
-        <div>
-            {isOpen && canEdit()
-                ? <>
-                    <form onSubmit={saveShelf}>
-                        <div><input name="name" defaultValue={shelf.name} /></div>
-                        <div><textarea name="description" defaultValue={shelf.description} /></div>
-                        <div><input name="public" id="public" type="checkbox" defaultChecked={shelf.public} /> <label htmlFor="public">Public shelf</label></div>
-                        <button>Save</button>
-                    </form>
-                    <button onClick={() => setIsOpen(false)}>Cancel</button>
-                </>
+        <>
+            <div>
+                {isOpen && isAuthor()
+                    ? <>
+                        <form onSubmit={saveShelf}>
+                            <div><input name="name" defaultValue={shelf.name} /></div>
+                            <div><textarea name="description" defaultValue={shelf.description} /></div>
+                            <div><input name="public" id="public" type="checkbox" defaultChecked={shelf.public} /> <label htmlFor="public">Public shelf</label></div>
+                            <button>Save</button>
+                        </form>
+                        <button onClick={() => setIsOpen(false)}>Cancel</button>
+                    </>
 
-                : <>
-                    <h2>{shelf.name}</h2>
-                    <div>DESCRIPTION: {shelf.description}</div>
-                    <div>AUTHOR: {isAuthor() ? "you" : shelf.author.name}</div>
+                    : <>
+                        <h2>{shelf.name}</h2>
+                        <div>DESCRIPTION: {shelf.description}</div>
+                        <div>AUTHOR: {isAuthor() ? "you" : shelf.author.name}</div>
 
-                    {canEdit() && <div><button onClick={() => setIsOpen(true)}>Edit</button></div>}
-                </>
-            }
-            {shelf.sharedWith && <ShelfSharing setShelf={() => { }} token={token} shelf={shelf} isAuthor={isAuthor()} />}
-            <h3>Records:</h3>
-            <table>
-                <tbody>
-                    {shelf.records.map(record =>
-                        <ShelfRecord
-                            key={record.record.id}
-                            record={record}
-                            canEdit={canEdit()}
-                        />)}
-                </tbody>
-            </table>
-        </div>
+                        {isAuthor() && <div><button onClick={() => setIsOpen(true)}>Edit</button></div>}
+                    </>
+                }
+            </div>
+            <Tabs titles={["Records | ", "Share with"]}>
+                <Tab>
+
+                    <h3>Records:</h3>
+                    <table>
+                        <tbody>
+                            {shelf.records.map(record =>
+                                <ShelfRecord
+                                    key={record.record.id}
+                                    record={record}
+                                    canEdit={canEdit()}
+                                />)}
+                        </tbody>
+                    </table>
+                </Tab>
+                <Tab>
+                    {shelf.sharedWith && <ShelfSharing setShelf={() => { }} token={token} shelf={shelf} isAuthor={isAuthor()} />}
+                </Tab>
+            </Tabs>
+        </>
     );
 };
 
