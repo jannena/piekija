7b5376d84c1a04711d0e8b5bd75a51b11bad8c14
jannena <janne.jalone@outlook.com>
2019-09-29 14:30:35 +0300

    Added barcode to item form in frontend

--- client/components/staff/RecordItems.js
+++ client/components/staff/RecordItems.js
@@ -17,14 +17,15 @@ const RecordItems = ({ items, locations, loantypes, addItem, getLocations, getLo
 
     const handleCreateItem = e => {
         e.preventDefault();
-        const { loantype, location, state, note } = e.target;
-        addItem(loantype.value, location.value, state.value, note.value);
+        const { barcode, loantype, location, state, note } = e.target;
+        addItem(loantype.value, location.value, state.value, note.value, barcode.value);
     };
 
     return (
         <>
             <div>
                 <form onSubmit={handleCreateItem}>
+                    <input name="barcode" placeholder="barcode" />
                     <Select name="loantype" options={loantypes.map(loantype => [loantype.name, loantype._id])} />
                     <Select name="location" options={locations.map(location => [location.name, location.id])} />
                     <input placeholder="state" name="state" />
@@ -34,10 +35,18 @@ const RecordItems = ({ items, locations, loantypes, addItem, getLocations, getLo
             </div>
             <button>Add item</button>
             <table style={{ width: "100%" }}>
+                <thead>
+                    <tr>
+                        <th>barcode</th>
+                        <th>location</th>
+                        <th>state</th>
+                        <th>note</th>
+                    </tr>
+                </thead>
                 <tbody>
                     {items.map(item =>
                         <tr key={item._id}>
-                            <td>{item._id}</td>
+                            <td>{item.barcode}</td>
                             <td>{item.location.name}</td>
                             <td>{item.state}</td>
                             <td>{item.note}</td>
