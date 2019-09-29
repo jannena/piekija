5e1c50081a18ae891c9e14509c45eae68cf6733d
jannena <janne.jalone@outlook.com>
2019-09-29 14:15:01 +0300

    Added endpoints for updating and deleting

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -3,6 +3,7 @@ const Item = require("../models/Item");
 const Record = require("../models/Record");
 
 itemRouter.get("/", (req, res, next) => {
+    // TODO: authorization
     Item
         .find({})
         .populate("record", { title: 1, author: 1 })
@@ -15,9 +16,10 @@ itemRouter.get("/", (req, res, next) => {
 });
 
 itemRouter.post("/", async (req, res, next) => {
+    // TODO: authorization
     const { record, location, loantype, state, note } = req.body;
     if (!record || !location || !loantype || !state)
-        return res.status(400).json({ error: "record or location or loanType or state is missing" });
+        return res.status(400).json({ error: "record or location or loantype or state is missing" });
 
     const newItem = new Item({
         record,
@@ -42,6 +44,43 @@ itemRouter.post("/", async (req, res, next) => {
         await recordOfItem.save();
 
         res.status(201).json(savedItem);
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+itemRouter.put("/:id", (req, res, next) => {
+    // TODO: authorization
+    const { id } = req.params;
+    const { location, loantype, state, note } = req.body;
+    console.log(location, loantype, state, note);
+    if (!location || !loantype || !state || note === undefined)
+        return res.status(400).json({ error: "location or loantype or state or note is missing" });
+
+    const updatedItem = {
+        location,
+        loantype,
+        state,
+        note
+    };
+
+    Item
+        .findByIdAndUpdate(id, updatedItem, { new: true })
+        .then(result => {
+            res.json(result);
+        })
+        .catch(next);
+});
+
+itemRouter.delete("/:id", async (req, res, next) => {
+    // TODO: authorization
+    const { id } = req.params;
+
+    try {
+        const item = await Item.findByIdAndRemove(id);
+        await Record.findByIdAndUpdate(item.record, { $pull: { items: id } });
+        res.status(204).end();
     }
     catch (err) {
         next(err);
