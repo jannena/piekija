652ef0b99f01d419b6dc329f25e9be2501930fcc
Jannena <janne.jalone@outlook.com>
2019-10-09 13:57:10 +0300

    Updated login tests

--- server/tests/api/login.test.js
+++ server/tests/api/login.test.js
@@ -4,39 +4,108 @@ const supertest = require("supertest");
 
 const api = supertest(app);
 
-const { addUserToDb, clearDatabase } = require("./testutils");
+const { addUserToDb, clearDatabase, generateCode } = require("./testutils");
 
-describe("when there is users in the db", () => {
+let tfaCode = "";
+
+describe("when there is users in the db (login tests)", () => {
     beforeAll(async () => {
         await clearDatabase();
         await addUserToDb("root", "salasana", false);
+        tfaCode = (await addUserToDb("2fauth", "salissana", false, true)).TFACode;
     });
 
-    test("can log in with correct credentials and return token", async () => {
-        const res = await api
-            .post("/api/login")
-            .send({
-                username: "root",
-                password: "salasana"
+    describe("and 2 factor authentication is enabled", () => {
+        describe("and code is not given", () => {
+            test("server returns code request message", async () => {
+                const result = await api
+                    .post("/api/login")
+                    .send({
+                        username: "2fauth",
+                        password: "salissana"
+                    })
+                    .expect(400)
+                    .expect("Content-type", /application\/json/);
+
+                expect(result.body.error).toBe("code needed");
             })
-            .expect(200)
-            .expect("Content-type", /application\/json/);
+        });
 
-        expect(res.body.token).toBeDefined();
+        describe("and code is given", () => {
+            test("user can log in with correct credentials and code", async () => {
+                const code = generateCode(tfaCode);
+                const result = await api
+                    .post("/api/login")
+                    .send({
+                        username: "2fauth",
+                        password: "salissana",
+                        code
+                    })
+                    .expect(200)
+                    .expect("Content-type", /application\/json/);
+
+                expect(result.body.token).toBeDefined();
+            });
+            test("user cannot log in with incorrect credentials and correct code", async () => {
+                const code = generateCode(tfaCode);
+                const result = await api
+                    .post("/api/login")
+                    .send({
+                        username: "2fauth",
+                        password: "salissanaonvaara!!!Hehe.",
+                        code
+                    })
+                    .expect(401)
+                    .expect("Content-type", /application\/json/);
+
+                expect(result.body.token).not.toBeDefined();
+                expect(result.body.error).toBe("wrong username or password");
+            });
+            test("user cannot log in with correct credentials and incorrect code", async () => {
+                const result = await api
+                    .post("/api/login")
+                    .send({
+                        username: "2fauth",
+                        password: "salissana",
+                        code: "177837"
+                    })
+                    .expect(401)
+                    .expect("Content-type", /application\/json/);
+
+                expect(result.body.error).toBe("invalid code");
+            });
+        })
     });
 
-    test("cannot log in with invalid credentials", async () => {
-        const res = await api
-            .post("/api/login")
-            .send({
-                username: "anneli",
-                password: "salasana"
-            })
-            .expect(401)
-            .expect("Content-type", /application\/json/);
+    describe("and 2 factor authentication is disabled", () => {
+        test("can log in with correct credentials without token and return token", async () => {
+            const res = await api
+                .post("/api/login")
+                .send({
+                    username: "root",
+                    password: "salasana"
+                })
+                .expect(200)
+                .expect("Content-type", /application\/json/);
 
-        expect(res.body.error).toBe("wrong username or password");
+            expect(res.body.token).toBeDefined();
+        });
+
+        test("cannot log in with invalid credentials", async () => {
+            const res = await api
+                .post("/api/login")
+                .send({
+                    username: "anneli",
+                    password: "salasana"
+                })
+                .expect(401)
+                .expect("Content-type", /application\/json/);
+
+            expect(res.body.error).toBe("wrong username or password");
+        });
     });
+
+
 });
 
 afterAll(() => {
--- server/tests/api/testutils.js
+++ server/tests/api/testutils.js
@@ -5,6 +5,7 @@ const Location = require("../../models/Location");
 const Shelf = require("../../models/Shelf");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const otp = require("speakeasy");
 
 const initMARC21Data = [
     "02021cam a22005774i 45000010008000000030011000080050017000190080041000360200031000770350025001080350026001330400035001590410013001940420009002070800030002160840014002460840019002601000032002792400026003112450060003372640057003972640011004542640011004653000023004763360028004993370049005273380025005766000031006016500026006326500036006586500032006946500020007266500036007466550049007826550036008316550029008676550023008966550079009197000060009988520015010588520015010738520015010888520015011038520015011188520015011338520015011488520014011638520015011778560126011928560125013181706985ANDL10001320181011151338.4180223t20182018fi ||||g      |0| f fin    a978-951-0-42529-9qsidottu  a(FI-KV)9789510425299  a(FI-MELINDA)012207067  aFI-NLbfinerdadFI-BTJdFI-KM1 afinheng  afinb1 a820x-321974/fin/fennica  a84.22ykl9 aJännitys2ykl1 aTudor, C. J.,ekirjoittaja.14aThe chalk man,lsuomi10aLiitu-ukko /cC. J. Tudor ; suomentanut Raimo Salminen. 1aHelsinki :bWerner Söderström osakeyhtiö,c[2018] 3aEU:ssa 4c©2018  a426 sivua ;c23 cm  atekstibtxt2rdacontent  akäytettävissä ilman laitettabn2rdamedia  anidebnc2rdacarrier04aEddiec(fiktiivinen hahmo) 7asalaisuudet2kaunokki 7aviestitxpiirustukset2kaunokki 7adéjà vu -ilmiö2kaunokki 7amurha2kaunokki 7aaikatasoty1986y20162kaunokki 7apsykologinen jännityskirjallisuus2kaunokki 7ajännityskirjallisuus2kaunokki 7aesikoisteokset2kaunokki 7aromaanit2kaunokki 7akaunokirjallisuusxenglanninkielinen kirjallisuusxkäännökset2kaunokki1 aSalminen, Raimo,ekääntäjä.0(FI-ASTERI-N)000058058  a18b001c3  a18b003c3  a18b201c3  a19b401c3  a18b501c3  a10b601c3  a12b701c3  a9b801c3  a18b901c342qimage/jpeguhttp://data.kirjavalitys.fi/data/servlets/ProductRequestServlet?action=getimage&ISBN=9789510425299zKansikuva42qtext/htmluhttp://data.kirjavalitys.fi/data/servlets/ProductRequestServlet?action=showreferat&ISBN=9789510425299zKuvaus",
@@ -16,18 +17,23 @@ const escapedMARC21Data = [
     "02470njm_a22006134i_4500001000800000003001100008005001700019007001500036008004100051024001800092028001600110035002000126035002600146040000800172041001300180084001800193084001700211110002800228245003000256246002500286246002900311260005800340300008700398336003900485337002300524338003100547500003800578505004100616511035000657596001001007650003901017650003301056650004901089650002901138650003301167650005101200700003101251852001501282852001501297852001501312979003601327979003501363979003701398979004201435979004601477979003501523979003501558979004901593979003501642979005601677979004601733979004001779979003701819\u001E1247533\u001EANDL100013\u001E20180613110936.0\u001Esd |||g|||m||d\u001E111129s2011    fi rc||__||||||__ | eng||\u001E3 \u001Fa6430043790028\u001E02\u001FaSCENECD-002\u001E  \u001Fa(FI-BTJ)1652326\u001E  \u001Fa(FI-MELINDA)007301531\u001E  \u001Fbfin\u001E0 \u001Fdeng\u001Fdfin\u001E  \u001Fa78.89112\u001F2ykl\u001E  \u001Fa78.8911\u001F2ykl\u001E2 \u001FaNightwish,\u001Feesitt\u00E4j\u00E4.\u001E10\u001FaImaginaerum \/\u001FcNightwish.\u001E33\u001FaImaginaerum original\u001E33\u001FaImaginaerum instrumental\u001E  \u001Fa[Kustannuspaikka tuntematon] :\u001FbScene Nation,\u001Fcp2011.\u001E  \u001Fa2 CD-\u00E4\u00E4nilevy\u00E4 (74 min 56 s, 73 min 56 s) +\u001Fetekstiliite (24 s.) + 2 julistetta\u001E  \u001Faesitetty musiikki\u001Fbprm\u001F2rdacontent\u001E  \u001Faaudio\u001Fbs\u001F2rdamedia\u001E  \u001Fa\u00E4\u00E4nilevy\u001Fbsd\u001F2rdacarrier\u001E  \u001FaLaulujen sanat tekstiliitteess\u00E4.\u001E0 \u001FaCD 1: Original ; CD 2: Instrumental.\u001E0 \u001FaNightwish: Anette Olzon (voc), Marco Hietala (b, voc), Emppu Vuorinen (g), Jukka Nevalainen (dr, perc), Tuomas Holopainen (keys, p) & The Metro Voices, The Young Musicians London, The Looking Glass Orchestra, Pekka Kuusisto (v), Troy Donockley (s\u00E4kkipilli, low whistle, bodhran, buzuki), Dermot Crehan (hardangerviulu), Dirk Campbell (sorna)...\u001E  \u001FbLO506\u001E 7\u001Faheavy rock\u001FzSuomi\u001Fy2010-luku\u001F2musa\u001E 7\u001Farock\u001FzSuomi\u001Fy2010-luku\u001F2musa\u001E 7\u001Faprogressiivinen rock\u001FzSuomi\u001Fy2010-luku\u001F2musa\u001E 7\u001Fakuorot\u001Fxheavy rock\u001F2musa\u001E 7\u001Faorkesterit\u001Fxheavy rock\u001F2musa\u001E 7\u001Fakelttil\u00E4inen kansanmusiikki\u001Fxheavy rock\u001F2musa\u001E1 \u001FaShearman, James,\u001Fejohtaja.\u001E  \u001Fa18\u001Fb001\u001Fc4\u001E  \u001Fa19\u001Fb401\u001Fc4\u001E  \u001Fa18\u001Fb501\u001Fc4\u001E  \u001Faanders.1261392\u001FbTaikatalvi\u001Fhfin\u001E  \u001Faanders.1261393\u001FbStorytime\u001Fheng\u001E  \u001Faanders.1261394\u001FbGhost river\u001Fheng\u001E  \u001Faanders.1261395\u001FbSlow, love, slow\u001Fheng\u001E  \u001Faanders.1261396\u001FbI want my tears back\u001Fheng\u001E  \u001Faanders.1261397\u001FbScaretale\u001Fheng\u001E  \u001Faanders.1261398\u001FbArabesque\u001Fheng\u001E  \u001Faanders.1261399\u001FbTurn loose the mermaids\u001Fheng\u001E  \u001Faanders.1261400\u001FbRest calm\u001Fheng\u001E  \u001Faanders.1261401\u001FbThe crow, the owl and the dove\u001Fheng\u001E  \u001Faanders.1261402\u001FbLast ride of the day\u001Fheng\u001E  \u001Faanders.1261403\u001FbSong of myself\u001Fheng\u001E  \u001Faanders.1261404\u001FbImaginaerum\u001Fheng\u001E\u001D"
 ];
 
-const addUserToDb = async (username, password, staff) => {
+const addUserToDb = async (username, password, staff, tfa) => {
     const passwordHash = await bcrypt.hash(password, 13);
-    const newUser = new User({
+    const newUserData = {
         username,
         passwordHash,
         name: "user",
         staff,
-        barcode: "asd",
+        barcode: "asd" + Math.random(),
         shelves: [],
-        loans: []
-    });
-    return await newUser.save();
+        loans: [],
+    };
+    const secret = otp.generateSecret({ encoding: "base32" }).base32;
+    if (tfa) newUserData.TFACode = secret;
+    const newUser = new User(newUserData);
+    const user = await newUser.save();
+    if (tfa) return { user, TFACode: secret };
+    else return user;
 };
 
 const addRecordToDb = async () => {
@@ -157,6 +163,13 @@ const recordsInDb = async () => (await Record.find({})).length;
 const locationsInDb = async () => (await Location.find({})).length;
 const shelvesInDb = async () => (await Shelf.find({})).length;
 
+const generateCode = secret => {
+    return otp.totp({
+        secret,
+        encoding: "base32"
+    });
+};
+
 module.exports = {
     addUserToDb,
     addRecordToDb,
@@ -178,5 +191,7 @@ module.exports = {
     usersInDb,
     recordsInDb,
     locationsInDb,
-    shelvesInDb
+    shelvesInDb,
+
+    generateCode
 };
