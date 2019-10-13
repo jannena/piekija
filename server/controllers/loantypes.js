a267b2b313d328b15c5ae5c6f626f659969266eb
Jannena <janne.jalone@outlook.com>
2019-10-13 21:08:51 +0300

    Updated loantype controller

--- server/controllers/loantypes.js
+++ server/controllers/loantypes.js
@@ -11,11 +11,7 @@ loantypeRouter.get("/", (req, res, next) => {
 loantypeRouter.post("/", (req, res, next) => {
     const { name, canBePlacedAHold, canBeLoaned, renewTimes, loanTime } = req.body;
     if (!name || canBePlacedAHold === undefined || canBeLoaned === undefined || renewTimes === undefined || loanTime === undefined)
-        return res.status(400).json(
-            {
-                error: "name or canBePlacedAHold or canBeLoaned or renewTimes or loanTime is missing"
-            }
-        );
+        return res.status(400).json({ error: "name or canBePlacedAHold or canBeLoaned or renewTimes or loanTime is missing" });
 
     const newLoantype = new Loantype({
         name,
@@ -27,6 +23,36 @@ loantypeRouter.post("/", (req, res, next) => {
     newLoantype
         .save()
         .then(result => void res.status(201).json(result))
+        .catch(next);
+});
+
+loantypeRouter.put("/:id", (req, res, next) => {
+    const { id } = req.params;
+
+    const { name, canBePlacedAHold, canBeLoaned, renewTimes, loanTime } = req.body;
+    if (!name || canBePlacedAHold === undefined || canBeLoaned === undefined || renewTimes === undefined || loanTime === undefined)
+        return res.status(400).json({ error: "name or canBePlacedAHold or canBeLoaned or renewTimes or loanTime is missing" });
+
+    const updatedLoanType = {
+        name,
+        canBePlacedAHold,
+        canBeLoaned,
+        renewTimes,
+        loanTime
+    };
+    Loantype
+        .findByIdAndUpdate(id, updatedLoanType, { new: true })
+        .then(result => res.json(result))
+        .catch(next);
+});
+
+loantypeRouter.delete("/:id", (req, res, next) => {
+    // TODO: Authorization
+    const { id } = req.params;
+
+    Loantype
+        .findByIdAndRemove(id)
+        .then(() => res.status(204).end())
         .catch(next);
 });
 
