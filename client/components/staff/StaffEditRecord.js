b14ae18e8e3f3ed74b6e7d9d08cfef7c14895c46
jannena <janne.jalone@outlook.com>
2019-12-07 16:41:54 +0200

    Added speed dial buttons to record editing screen

--- client/components/staff/StaffEditRecord.js
+++ client/components/staff/StaffEditRecord.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Tabs, Tab, AddressedTabs } from "../Tabs";
+import { Tab, AddressedTabs } from "../Tabs";
 import { connect } from "react-redux";
 import { getRecord, removeRecord, createRecord } from "../../reducers/recordReducer";
 import MARCEditor from "./MARCEditor";
@@ -7,9 +7,10 @@ import Record from "../record/Record";
 import Loader from "../Loader";
 import RecordItems from "./RecordItems";
 import SimpleRecordEditor from "./SimpleRecordEditor";
+import { withRouter } from "react-router-dom";
 import __ from "../../langs";
 
-const StaffEditRecord = ({ state, id, record, getRecord, removeRecord, createRecord, __ }) => {
+const StaffEditRecord = ({ state, id, record, getRecord, removeRecord, createRecord, history, __ }) => {
 
     useEffect(() => {
         console.log(id);
@@ -18,12 +19,13 @@ const StaffEditRecord = ({ state, id, record, getRecord, removeRecord, createRec
     }, [id]);
 
     if (state.state === 1) return <Loader />
-    if (state.state === 3) return <p>Error: {state.error}</p>;
+    if (state.state === 3) return <p>{__("Error")}: {state.error}</p>;
 
     const hasItems = record.result && record.result.items && record.result.items.length !== 0
     const isPreview = record && record.result && record.result.id === "preview";
 
     const onSaveToDatabase = () => {
+        // TODO: change page to record just created
         createRecord(record.result.record);
     };
 
@@ -31,8 +33,17 @@ const StaffEditRecord = ({ state, id, record, getRecord, removeRecord, createRec
         removeRecord();
     };
 
+    const recordTools = () => {
+        if (record && record.result && record.result.id && record.result.id === "preview") return <button onClick={onSaveToDatabase}>{__("Save to database")}</button>;
+        else return <>
+            <button onClick={() => history.push(`/staff/records`)}>{__("Back to staff screen")}</button>
+            <button onClick={() => history.push(`/record/${id}`)}>{__("Preview as normal user")}</button>
+        </>;
+    };
+
     return <>
-        {(record && record.result && record.result.id && record.result.id === "preview") && <button onClick={onSaveToDatabase}>{__("Save to database")}</button>}
+        {recordTools()}
+
         <AddressedTabs titles={[__("preview-tab"), __("items-tab"), __("MARC-tab"), __("simple-editor-tab"), __("remove-tab")]}
             addresses={["", "items", "marc", "simple", "remove"]}
             root={`staff/record/${id}`}>
@@ -64,4 +75,4 @@ export default connect(
         __: __(state)
     }),
     { getRecord, removeRecord, createRecord }
-)(StaffEditRecord);
+)(withRouter(StaffEditRecord));
