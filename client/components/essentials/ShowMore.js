05aa1a1fcdc0dfa3130d11ec0b8feac788e227af
jannena <janne.jalone@outlook.com>
2020-01-03 20:33:10 +0200

    Updated ShowMore

--- client/components/essentials/ShowMore.js
+++ client/components/essentials/ShowMore.js
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 
-const ShowMore = ({ data, show }) => {
+const ShowMore = ({ data, show, noDiv, buttonContainer }) => {
     const [showAll, setShowAll] = useState(false);
 
     const showDefault = data.slice(0, show);
     const rest = data.slice(show);
+
+    const buttonStyle = {
+        margin: "auto",
+        marginTop: 10,
+        marginBottom: 10,
+        maxWidth: 200,
+        width: "100%"
+    };
+
+    const button = <button style={buttonStyle} onClick={() => setShowAll(!showAll)}>{showAll ? <>&uarr; Hide</> : <>&darr; Show more</>}</button>;
+
+    if (noDiv) return <>
+        {showDefault}
+        {showAll && rest}
+        {buttonContainer(button)}
+    </>;
 
     return (<div>
         <div>
             {showDefault}
         </div>
         {showAll && <div>{rest}</div>}
-        <button onClick={() => setShowAll(!showAll)}>{showAll ? "Hide" : "Show"}</button>
+        {button}
     </div>);
 };
 
